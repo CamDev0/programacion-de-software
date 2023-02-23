8b0f97ce86c86547b197492a764130127221ef21
Camilo Barrios <camilobarriosdev@gmail.com>
2023-02-22 20:48:14 -0500

    Guardada la solucion

---
+++ CuentaBancaria.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class CuentaBancaria
+{
+	string Nombre, Apellido, Direccion, NIT;
+	double Saldo;
+
+	//constructor
+	public CuentaBancaria(string NombrePA, string ApellidoPA, string DireccionPA, string NITPA, double saldoPA )
+	{
+		Nombre = NombrePA;
+		Apellido = ApellidoPA;	
+		Direccion= DireccionPA;	
+		NIT = NITPA;
+		Saldo = saldoPA;
+	}
+
+	public double Deposito(double moreMoney)
+	{
+		Saldo += moreMoney;
+        Console.WriteLine("\nOperación realizada con éxito.\n");
+        return Saldo;
+	}
+
+    public double Retiro (double lessMoney)
+    {
+		if (Saldo >= lessMoney)
+		{
+			Saldo -= lessMoney;
+			Console.WriteLine("\nOperación realizada con éxito.\n");
+			return Saldo;
+		}
+		else
+		{
+			Console.WriteLine("\nEl monto a retirar supera su ahorro, por favor verifique su saldo.\n");
+			return Saldo;
+		}
+
+    }
+
+    public void ConsultaSaldo()
+    {
+		Console.Write("Su saldo actual es de: " + Saldo + "\n\n");
+    }
+
+	public string ToString()
+	{
+		string datos = ("\nNombre: " + Nombre + "\nApellido: " + Apellido + "\nDirección: " + Direccion + "\nNIT: " + NIT + "\n" );
+		Console.WriteLine(datos);
+		return datos;
+	}
+
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿
+
+namespace ejercicioPOO
+{
+    class Principal
+    {
+        public static void Main(string[] args)
+        {
+            string Nombre, Apellido, Direccion, NIT;
+            double Saldo;
+            int num;
+
+            Console.WriteLine("Ingrese el Nombre: ");
+            Nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el Apellido: ");
+            Apellido = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la Dirección: ");
+            Direccion = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el NIT: ");
+            NIT = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el saldo inicial de su cuenta: ");
+            Saldo = Double.Parse(Console.ReadLine());
+
+            CuentaBancaria lector = new CuentaBancaria(Nombre, Apellido, Direccion, NIT, Saldo);
+            
+            Console.Write("\n ********************\n\n");
+            
+            do
+            {
+
+            Console.WriteLine("1.Deposito" + "\n" + 
+                              "2.Retiro" + "\n" +
+                              "3.Consultar Saldo" + "\n" + 
+                              "4.Consultar Datos" +"\n" + 
+                              "5. Salir" + "\n");
+            num = Int32.Parse(Console.ReadLine());
+                
+                switch (num)
+                {
+
+                 case 1: Console.Write("*** DEPOSITO ***\n\n");
+                        Console.WriteLine("Ingrese el saldo a depositar: ");
+                        double newSaldo = Double.Parse(Console.ReadLine());
+                        lector.Deposito(newSaldo);
+                        break;
+
+                case 2:
+                        Console.Write("*** RETIRO *** \n\n");
+                        Console.WriteLine("Ingrese el saldo a retirar: ");
+                        double newSaldoR = Double.Parse(Console.ReadLine());
+                        lector.Retiro(newSaldoR);
+                        break;
+
+                case 3:
+                        Console.Write("*** CONSULTA DE SALDO ***\n\n");
+                        lector.ConsultaSaldo();
+                        break;
+
+                case 4:
+                        Console.Write("*** CONSULTA DE DATOS ***\n\n ");
+                        lector.ToString();
+                        break;
+
+                }
+
+            } while (num != 5);
+            
+            
+        }
+    }
+}
